b92b3f885a3ab374739727538a82b86df30b85d8
Etienne Delay <etienne.delay@cirad.fr>
2023-06-09 19:02:18 +0000

    update avec les zone d'intérêt

--- scriptR/analyse_pse.R
+++ scriptR/analyse_pse.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(ggforce)
+library(plotly)
 rm(list = ls())
 
 
@@ -7,9 +9,14 @@ pse.df <- read.csv("results_pse/population33200.csv", header = T)
 
 sel <- pse.df$evolution.samples > 4
 
-ggplot(data = pse.df[sel,])+
-  geom_point(aes(x = objective.om_stockMil, y = objective.om_charette, 
+
+ggplot()+
+  geom_point(data = pse.df[sel,], aes(x = objective.om_stockMil, y = objective.om_charette, 
                  size = evolution.samples, colour =  nbProTGMax))+
+  geom_rect(data=mtcars[1,], aes(xmin=51200, xmax=52300, ymin=24,ymax=28), colour = "#252525",fill="#d9d9d9", alpha=0.1)+
+  geom_rect(data=mtcars[1,], aes(xmin=41800, xmax=43000, ymin=41.5,ymax=45), colour = "#252525",fill="#d9d9d9", alpha=0.1)+
+  annotate( "text", label = "1", x = 52500, y = 29, size = 8, colour = "#252525")+
+  annotate( "text", label = "2", x = 43000, y = 47, size = 8, colour = "#252525")+
   labs(title = "Pattern Space Exploration",
        x = "Prod. de mil", 
        y = "Prod. bois de chauffe")+
@@ -19,6 +26,18 @@ ggplot(data = pse.df[sel,])+
 
 ggsave("../img/om_pse.png", width = 8)
 ggsave("../img/om_pse.svg", width = 8)
+
+## Test de ploty
+
+fig <- plot_ly(pse.df[sel,], x = ~objective.om_stockMil, y = ~objective.om_charette,
+               text = ~paste('TempChamps:', tpsAuChamp, 
+                             '<br>probaDiscu.1:', probaDiscu,
+                             '<br>fréquenceRéu',fréquenceRéu,
+                             '<br>probaDenonce',probaDenonce,
+                             '<br>nbProTGMax',nbProTGMax)
+               , type = 'scatter', mode = 'markers',
+               marker = list(size = ~evolution.samples*2, opacity = 0.5))
+fig
 
 # columns_to_remove <- grep(".1", names(pse.df))
 # pse.df <- pse.df[,-columns_to_remove]
